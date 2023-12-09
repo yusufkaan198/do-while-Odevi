9e40b822dfdd3591909050d7f03b4eca50ad0974
yusuf <yusuf@HP_VICTUS-16>
2023-12-09 18:16:42 +0300

    Add project files.

---
+++ ProgramlamaOdev3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramlamaOdev3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x= 0, y= 0;
+            do
+            {
+                Console.Write($"({x},{y}) ");
+                y++;
+            } while (y < 2);
+            Console.WriteLine();
+            x++;
+            y = 0;
+            do
+            {
+                Console.Write($"({x},{y}) ");
+                y++;
+            } while (y < 2);
+            Console.ReadLine();
+        }
+    }
+}
